3a512eb38370570cc372a8f832a692b839d3f06a
Andoniaina516 <ando.nomenjanahary@futurmap.com>
2023-01-26 13:57:23 +0300

    Adding load model from local or github function.

--- realtime_detect.py
+++ realtime_detect.py
@@ -2,14 +2,19 @@ import cv2 as cv
 import torch
 import numpy as np
 
-def load_model(model):
-    model = torch.hub.load('ultralytics/yolov5', model, pretrained=True)
+
+def load_model(model, source : str = 'local' or 'github'):
+    if source == 'github':
+        model = torch.hub.load('ultralytics/yolov5', model, pretrained=True)
+    elif source == 'local':
+        model = torch.hub.load('D:\DEVELOPPEMENTS\MACHINE_LEARNING\yolov5', model, source='local', pretrained=True)
     return model
+
 
 if __name__ == '__main__':
     
     # Load model
-    model = load_model('yolov5s')
+    model = load_model('yolov5s', source='local')
     
     cap = cv.VideoCapture(0)
 
